41d25a5301c191b461645da447b65a55276dbbf9
ism1762 <ism1762@rit.edu>
2022-02-11 18:16:51 -0500

    final commit 1

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
@@ -44,7 +44,18 @@ public class InventoryController {
         this.inventoryDAO = inventoryDAO;
     }
 
-    /**
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        return null ;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Product[]> getProducts() {
+        return null ;
+    }
+
+
+   /**
      * Responds to the GET request for all {@linkplain Hero heroes}
      * 
      * @author Isaac S McKinney
@@ -60,6 +71,65 @@ public class InventoryController {
             return new ResponseEntity<>(products, HttpStatus.OK);
         } catch (IOException e) {
             LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+
+    /**
+     * Creates a {@linkplain Product product} with the provided product object
+     * 
+     * @param product - The {@link Product product} to create
+     * 
+     * @return ResponseEntity with created {@link Product product} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link Product product} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PostMapping("")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        LOG.info("POST /heroes " + product);
+
+        try {
+            Product newProduct = inventoryDAO.createProduct(product) ;
+            if(newProduct != null)
+                return new ResponseEntity<>(newProduct, HttpStatus.CREATED) ;
+            else
+                return new ResponseEntity<>(product, HttpStatus.CONFLICT) ;
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @PutMapping("")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        return null ;
+    }
+    
+    /**
+     * Deletes a {@linkplain Hero hero} with the given id
+     * 
+     * @param id The id of the {@link Hero hero} to deleted
+     * 
+     * @return ResponseEntity HTTP status of OK if deleted<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable int id) {
+        LOG.info("DELETE /products/" + id);
+
+        try {  // delete product
+            boolean bool = inventoryDAO.deleteProduct(id);
+
+            if (bool) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+        } catch (IOException e) {  // storage issue
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
