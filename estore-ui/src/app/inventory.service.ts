f4ba5c99a7fe00d587d14e07dfc2c8647ce0adac
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-20 16:08:49 -0500

    stubbed out methods

--- estore-ui/src/app/inventory.service.ts
+++ estore-ui/src/app/inventory.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
+import { Product } from './product';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//import {catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryService {
 
-  constructor() { }
+    private productsUrl = 'http://localhost:8080/products';  // URL to web api
+
+    httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    constructor(private http: HttpClient) { }
+
+
+    //private handleError<T>(operation = 'operation', result?: T) { }
+
+
+    /**
+     * 
+     * @returns 
+     */
+    getInventory(): Observable<Product[]> { return new Observable; }
+
+
+    /**
+     * 
+     * @param id 
+     * @returns 
+     */
+    getProduct(id: number): Observable<Product> { return new Observable; }
+
+
+    /**
+     * 
+     * @param product 
+     * @returns 
+     */
+    updateProduct(product: Product): Observable<any> { return new Observable; }
+
+
+    /**
+     * 
+     * @param product 
+     * @returns 
+     */
+    addProduct(product: Product): Observable<Product> { return new Observable; }
+
+    /**
+     * 
+     * @param id 
+     * @returns 
+     */
+    deleteProduct(id: number): Observable<Product> { return new Observable; }
+
+
+    /**
+     * 
+     * @param prompt 
+     * @returns 
+     */
+    searchProducts(prompt: string): Observable<Product[]> { return new Observable; }
 }
