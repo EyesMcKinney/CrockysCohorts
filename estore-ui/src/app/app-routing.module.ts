5b516ac2fa6fa6d97ed4cc506ea7c7ab5d99b573
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-20 15:55:49 -0500

    init implementation for routing table

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
 
-
+const routes: Routes = [
+    { path: 'homepage', component: HomepageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'info/:id', component: ProductInfoComponent },
+    { path: '', redirectTo: '/homepage', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
