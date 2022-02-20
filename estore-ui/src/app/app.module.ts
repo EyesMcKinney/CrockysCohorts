b3de90310b5066b40a5d5f4f696b4fabfb4b8967
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-20 15:11:50 -0500

    updated with new components and services

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductSearchComponent,
+    ProductInfoComponent,
+    HomepageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
