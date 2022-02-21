801498912b5309e8fd84fd2712fd18ede202350d
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-21 13:26:02 -0500

    specified imports

--- estore-ui/src/app/product-info/product-info.component.ts
+++ estore-ui/src/app/product-info/product-info.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Product } from '../product';
+import { InventoryService } from '../inventory.service';
 
 @Component({
   selector: 'app-product-info',
