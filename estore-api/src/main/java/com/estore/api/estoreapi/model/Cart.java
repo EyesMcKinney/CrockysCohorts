ebee3f5bbc2c21f09d536157e118096ed7f973eb
ism1762 <ism1762@rit.edu>
2022-02-23 18:27:00 -0500

    Second Commit

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
@@ -2,4 +2,13 @@ package com.estore.api.estoreapi.model;
 
 public interface Cart {
 
+    public boolean addProduct(int id);
+
+    public boolean removeProduct(int id);
+
+    public Product[] getProducts();
+
+    public User getUser();
+
+    public boolean clearCart();
 }
--- estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
@@ -1,15 +1,16 @@
 package com.estore.api.estoreapi.model;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ShoppingCart {
 
-    @JsonProperty("shopping cart") private LinkedList shoppingCart;
+    @JsonProperty("shopping cart") private List<Product> products;
 
     public ShoppingCart(){
-        this.shoppingCart = new LinkedList<>();
+        this.products = new LinkedList<>();
     }
 
 }
