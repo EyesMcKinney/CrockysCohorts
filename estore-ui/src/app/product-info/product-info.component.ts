37d0121f3a5c12941114a1acefba898478c95744
Tylin Hartman <78936590+tbh2677@users.noreply.github.com>
2022-02-24 21:18:49 -0500

    Product Info (experimentation)

--- estore-ui/src/app/product-info/product-info.component.ts
+++ estore-ui/src/app/product-info/product-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { InventoryService } from '../inventory.service';
 
 @Component({
   selector: 'app-product-info',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductInfoComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [] ;
+
+  constructor(private inventoryService: InventoryService) { }
 
   ngOnInit(): void {
+    this.getProducts() ;
   }
 
+  getProducts(): void {
+    this.inventoryService.getInventory()
+    .subscribe(products => this.products = products);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.inventoryService.addProduct({ name } as Product)
+      .subscribe(product => {
+        this.products.push(product);
+      });
+  }
+
+  delete(product: Product): void {
+    this.products = this.products.filter(h => h !== product);
+    this.inventoryService.deleteProduct(product.id).subscribe();
+  }
 }
