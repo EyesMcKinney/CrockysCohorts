72ceafb82441bd03aacc47a2a770c021f1bb695e
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-26 14:54:56 -0500

    order of methods switched for legibility

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
@@ -45,35 +45,6 @@ public class LoginController {
 
 
     /**
-     * Responds to the GET request for a {@linkplain User User} for the given username
-     * 
-     * @param username The username used to locate the {@link User User}
-     * 
-     * @return ResponseEntity with {@link User User} object and HTTP status of OK if found<br>
-     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
-     */
-    @GetMapping("/{username}")
-    public ResponseEntity<User> getUser(@PathVariable String username) {
-        LOG.info("GET /login/" + username);
-
-        try {
-            User user = userDAO.getUser(username);
-
-            if (user != null) {
-                return new ResponseEntity<User>(user, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-            
-        } catch (IOException e) {
-            LOG.log(Level.SEVERE, e.getLocalizedMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-
-    /**
      * Creates a {@linkplain User User} with the provided product object
      * 
      * @param product - The {@link User User} to create
@@ -93,6 +64,35 @@ public class LoginController {
                 return new ResponseEntity<User>(newUser, HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(user, HttpStatus.CONFLICT);
+            }
+            
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    /**
+     * Responds to the GET request for a {@linkplain User User} for the given username
+     * 
+     * @param username The username used to locate the {@link User User}
+     * 
+     * @return ResponseEntity with {@link User User} object and HTTP status of OK if found<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        LOG.info("GET /login/" + username);
+
+        try {
+            User user = userDAO.getUser(username);
+
+            if (user != null) {
+                return new ResponseEntity<User>(user, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             
         } catch (IOException e) {
