82392b960b184fb46c91e980bc97b28f11adb7f6
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-26 14:20:24 -0500

    stubbed class

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
@@ -1,5 +1,17 @@
 package com.estore.api.estoreapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Handles the REST API requests for the {@linkplain User} resource.
+ * 
+ * The Spring annotation identifies this module as a REST API method handler.
+ * 
+ * @author Stevie Alvarez
+ */
+@RestController
+@RequestMapping("user_login")
 public class LoginController {
     
 }
