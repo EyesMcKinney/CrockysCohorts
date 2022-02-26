eabce69ddf5ff9def7fa0e4b6b14e41e1babb6b0
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-26 14:48:34 -0500

    createUser takes User as a param

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
@@ -18,10 +18,10 @@ public interface UserDAO {
     /**
      * Retrieve a {@linkplain User user} with the provided username.
      * 
-     * @param username The name identifying the desired {@link User user}
+     * @param user A {@link User user} to be created and stored
      * @return A {@link User user} with the matching username,
      * null if {@link User user} could not be found
      * @throws IOException If issue with underlying storage
      */
-    User getUser(String username) throws IOException;
+    User getUser(User user) throws IOException;
 }
