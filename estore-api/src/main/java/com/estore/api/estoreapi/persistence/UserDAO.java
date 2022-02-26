a1561521adca007cc43c00c7b8f332cd1feef1bf
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-26 14:30:13 -0500

    definition of necessary interface methods

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserDAO.java
@@ -6,6 +6,22 @@ import com.estore.api.estoreapi.model.User;
 
 public interface UserDAO {
     
-    User createUser(String text) throws IOException;
+    /**
+     * CREATE a {@linkplain User user} with the provided username.
+     * 
+     * @param username The username for the new user
+     * @return A new user with the provided username
+     * @throws IOException If issue with underlying storage
+     */
+    User createUser(String username) throws IOException;
 
+    /**
+     * Retrieve a {@linkplain User user} with the provided username.
+     * 
+     * @param username The name identifying the desired {@link User user}
+     * @return A {@link User user} with the matching username,
+     * null if {@link User user} could not be found
+     * @throws IOException If issue with underlying storage
+     */
+    User getUser(String username) throws IOException;
 }
