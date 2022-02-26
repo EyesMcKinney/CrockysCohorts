c414165a7746c6762021be7d3a1e963269131edb
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-26 14:22:58 -0500

    init fields and constructor

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.estore.api.estoreapi.controller;
 
+import java.util.logging.Logger;
+
+import com.estore.api.estoreapi.persistence.UserDAO;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Handles the REST API requests for the {@linkplain User} resource.
@@ -13,5 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user_login")
 public class LoginController {
-    
+    private static final Logger LOG = Logger.getLogger(InventoryController.class.getName());
+    private UserDAO userDAO;
+
+
+    /**
+     * 
+     * @param inventoryDAO The {@link UserDAO Product Data Access Object} to perform CRUD operations
+     * This dependency is injected by the Spring Framework
+     */
+    public LoginController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
 }
