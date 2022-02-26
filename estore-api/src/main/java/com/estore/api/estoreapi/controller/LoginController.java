2f92a5ae728f26954dd193f214d4a40f3ac63ac2
Stevie Alvarez <stevie.6strings@gmail.com>
2022-02-26 14:38:57 -0500

    documentation update

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/LoginController.java
@@ -22,6 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user_login")
 public class LoginController {
+    /**
+     * Log messages in the server via standard IO
+     */
     private static final Logger LOG = Logger.getLogger(InventoryController.class.getName());
     private UserDAO userDAO;
 
@@ -37,12 +40,29 @@ public class LoginController {
 
 
     /**
+     * Responds to the GET request for a {@linkplain User User} for the given username
      * 
-     * @param username
+     * @param username The username used to locate the {@link User User}
+     * 
+     * @return ResponseEntity with {@link User User} object and HTTP status of OK if found<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     public ResponseEntity<User> getUser(@PathVariable String username) {
         return null;
     }
 
-    public ResponseEntity<User> createUser(@RequestBody User user)
+
+    /**
+     * Creates a {@linkplain User User} with the provided product object
+     * 
+     * @param product - The {@link User User} to create
+     * 
+     * @return ResponseEntity with created {@link User User} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link User User} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return null;
+    }
 }
