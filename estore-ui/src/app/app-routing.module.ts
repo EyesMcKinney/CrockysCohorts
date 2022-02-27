534010c86475a7aef310ca455ab8a2edf61b0c82
Tylin Hartman <78936590+tbh2677@users.noreply.github.com>
2022-02-27 15:48:52 -0500

    trying to debug

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductInfoComponent } from './product-info/product-info.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
   { path: 'homepage', component: HomepageComponent },
   { path: 'loginpage', component: LoginpageComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'admin', component: AdminComponent},
   { path: 'info/:id', component: ProductInfoComponent },
 ];
 
