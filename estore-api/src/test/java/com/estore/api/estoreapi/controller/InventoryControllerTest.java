15a4c2f691513b2612dc6b90e53fafd957eb4154
Stevie Alvarez <stevie.6strings@gmail.com>
2022-03-01 20:42:54 -0500

    init testGetProduct implementation

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/InventoryControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/InventoryControllerTest.java
@@ -1,6 +1,10 @@
 package com.estore.api.estoreapi.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 
 import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.persistence.InventoryDAO;
@@ -9,6 +13,9 @@ import com.estore.api.estoreapi.persistence.InventoryFileDAO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Tag("Controller-tier")
 public class InventoryControllerTest {
@@ -21,6 +28,7 @@ public class InventoryControllerTest {
     // Test objects
     private final Product TEST_PRODUCT = new Product(11, "Crocy's Croc", 10.99, 2, "Crocy's Croc Description");
 
+
     /// run before each test
     @BeforeEach
     public void setup() {
@@ -28,10 +36,23 @@ public class InventoryControllerTest {
         inventoryController = new InventoryController(mockInventoryFileDAO);
     }
 
-
     /// run after each test
     @AfterEach
     public void cleanup() {
 
+    }
+
+
+    @Test
+    public void testGetProduct() throws IOException {
+        // setup
+        when(mockInventoryFileDAO.getProduct(TEST_PRODUCT.getId())).thenReturn(TEST_PRODUCT);
+
+        // invoke
+        ResponseEntity<Product> response = inventoryController.getProduct(TEST_PRODUCT.getId());
+
+        // check
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(TEST_PRODUCT, response.getBody());
     }
 }
