cabc49b5e336e3ee19fa82d72413a8edfb7c8a10
Stevie Alvarez <stevie.6strings@gmail.com>
2022-03-01 20:28:11 -0500

    init setup

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/InventoryControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/InventoryControllerTest.java
@@ -1,5 +1,34 @@
 package com.estore.api.estoreapi.controller;
 
+import com.estore.api.estoreapi.model.Product;
+import com.estore.api.estoreapi.persistence.InventoryDAO;
+import com.estore.api.estoreapi.persistence.InventoryFileDAO;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+
+@Tag("Controller-tier")
 public class InventoryControllerTest {
-    
+    // CuT (Conponent under Test)
+    private InventoryController inventoryController;
+
+    // Mock objects
+    private InventoryDAO mockInventoryFileDAO;
+
+    // Test objects
+    private final Product TEST_PRODUCT = new Product(11, "Crocy's Croc", 10.99, 2, "Crocy's Croc Description")
+
+    /// run before each test
+    @BeforeEach
+    public void setup() {
+
+    }
+
+
+    /// run after each test
+    @AfterEach
+    public void cleanup() {
+
+    }
 }
