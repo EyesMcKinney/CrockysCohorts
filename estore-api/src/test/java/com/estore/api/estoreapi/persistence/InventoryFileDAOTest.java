23628651d22d0372b349fa81446866244896edb4
ism1762 <ism1762@rit.edu>
2022-03-03 10:03:18 -0500

    documentaion done

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
@@ -15,8 +15,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-//TODO
-//Documentation
+/**
+ * InventoryFileDAOTest.java
+ * A file made for testing inventory file DAO
+ * @author: Isaac S McKinney
+ * 3/2/2022
+ */
 @Tag("Persistence-tier")
 public class InventoryFileDAOTest {
     //CuT
@@ -28,6 +32,7 @@ public class InventoryFileDAOTest {
     //Mock product list
     private Product[] testProducts;
 
+    // Set up the test equipment for the test functions to use
     @BeforeEach
     public void setup() throws StreamReadException, DatabindException, IOException{
         mockObjectMapper = mock(ObjectMapper.class);
@@ -40,6 +45,8 @@ public class InventoryFileDAOTest {
         mockFileDAO = new InventoryFileDAO("partyInTheUSA.txt", mockObjectMapper);
     }
 
+    // Test if InventoryFileDAO's getInventory works
+    // Get all the products from the set up and compare 
     @Test
     public void testGetInventory() throws IOException{
         Product[] products = mockFileDAO.getInventory();
@@ -51,6 +58,8 @@ public class InventoryFileDAOTest {
         }
     }
 
+    // Test if InventroyFileDAO's getProduct works
+    // Get a single product and check if it matches the first test product we made
     @Test 
     public void testGetProduct() throws IOException{
         Product result = mockFileDAO.getProduct(0);
@@ -58,6 +67,8 @@ public class InventoryFileDAOTest {
         assertEquals(testProducts[0], result);
     }
 
+    // Test if InventoryFileDAO's createProduct works
+    // See if we can create the product into our test products list and find it again
     @Test
     public void testCreate() throws IOException{
         Product product = new Product(3, "Crochet Quagsire", 999, 999, "Best Boy");
@@ -69,6 +80,8 @@ public class InventoryFileDAOTest {
         assertEquals(result.getName(), actual.getName());
     }
 
+    // Test if InventoryFileDAO's updateProduct works
+    // See if we can create a product and then update another product with it
     @Test
     public void testUpdate() throws IOException{
          Product product = new Product(0, "Scarf", 25, 50, "A cozy scarf");
@@ -79,6 +92,8 @@ public class InventoryFileDAOTest {
          assertEquals(actual, product);
     }
 
+    // Test if InventoryFileDAO's updateProduct fails or not
+    // see if the code returns the correct error response when given invalid items
     @Test
     public void testUpdateFail() throws IOException{
         Product product = new Product(99, "Scarf", 25, 50, "A cozy scarf");
@@ -87,6 +102,8 @@ public class InventoryFileDAOTest {
         assertNull(result);
     }
 
+    // Test if Save correctly throws an exception
+    // Save will throw an IOException since no file is found
     @Test
     public void testSaveException() throws StreamWriteException, DatabindException, IOException{
         doThrow(new IOException()).when(mockObjectMapper).writeValue(any(File.class), any(Product[].class));
@@ -95,6 +112,8 @@ public class InventoryFileDAOTest {
         assertThrows(IOException.class, () -> mockFileDAO.createProduct(product), "Unhandled!");
     }
 
+    // Test if findProducts is working. 
+    // retruns one product
     @Test
     public void testFind() throws IOException{
         Product[] result = assertDoesNotThrow(() -> mockFileDAO.findProducts("Plush"), "Unhandled!");
@@ -105,6 +124,8 @@ public class InventoryFileDAOTest {
         assertEquals(result[0], actual[0]); 
     }
 
+    // Test if findProducts is working. 
+    // retruns multiple products
     @Test 
     public void testFindMultiple() throws IOException{
         Product[] result = assertDoesNotThrow(() -> mockFileDAO.findProducts("cozy"), "Unhandled!");
@@ -116,6 +137,9 @@ public class InventoryFileDAOTest {
         assertEquals(result[0], actual[0]); 
         assertEquals(result[1], actual[1]);
     }
+
+    // Test if deleteProduct works
+    // We select a product to delete and *poof* its gone.
     @Test
     public void testDelete() throws IOException{
         Boolean result = assertDoesNotThrow(() -> mockFileDAO.deleteProduct(testProducts[2].getId()), "Unhandled!");
@@ -124,6 +148,9 @@ public class InventoryFileDAOTest {
         assertEquals(actual, result);
     }
 
+    // Test if deleteProduct fails properly
+    // We can't delete a product for any reason (Ex. It doesn't exsist)
+    // a correct error is thrown
     @Test
     public void testDeleteFail() throws IOException{
         Boolean result = assertDoesNotThrow(() -> mockFileDAO.deleteProduct(99), "Unhandled!");
