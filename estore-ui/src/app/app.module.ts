02306d17b41d46a46cf824ece9beebbb1c209956
Stevie Alvarez <stevie.6strings@gmail.com>
2022-03-03 14:19:59 -0500

    generated header component

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
@@ -24,7 +25,8 @@ import { LoginpageComponent } from './loginpage/loginpage.component';
     ProductSearchComponent,
     ProductInfoComponent,
     HomepageComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    HeaderComponent
   ],
   //providers: [],
   bootstrap: [AppComponent]
