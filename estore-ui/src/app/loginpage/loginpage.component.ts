b7cd7f6a626a07a403a7f9e4504bf3c8fcdd47d3
Stevie Alvarez <stevie.6strings@gmail.com>
2022-03-03 14:06:35 -0500

    commented out parent/child data relationship

--- estore-ui/src/app/loginpage/loginpage.component.ts
+++ estore-ui/src/app/loginpage/loginpage.component.ts
@@ -11,7 +11,7 @@ import { Location } from '@angular/common';
 })
 export class LoginpageComponent implements OnInit {
   user?: User | undefined;
-  @Output() newUser = new EventEmitter<User>();
+  //@Output() newUser = new EventEmitter<User>();
   adminUser: Boolean | undefined; 
 
   constructor(private loginService: LoginService, private location: Location) {
@@ -26,7 +26,7 @@ export class LoginpageComponent implements OnInit {
    * Load previous page.
    */
   goBack(): void {
-      this.newUser.emit(this.user);  // <<<<<<<<<<<<<<< SEND USER DATA TO MAIN COMPONENT
+      //this.newUser.emit(this.user);  // <<<<<<<<<<<<<<< SEND USER DATA TO MAIN COMPONENT
       if (this.user?.username == "admin") { 
           return;  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ROUTE TO ADMIN COMPONENT
       } else {
