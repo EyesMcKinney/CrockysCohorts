84feb2c1c92715bb68633eeb9678225fb9a7231c
Stevie Alvarez <stevie.6strings@gmail.com>
2022-03-13 19:55:30 -0400

    init commit

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ShoppingCartDAO.java
@@ -0,0 +1,10 @@
+package com.estore.api.estoreapi.persistence;
+
+/**
+ * Interface for {@linkplain ShoppingCart ShoppingCart} object persistance
+ * 
+ * @author Stevie Alvarez
+ */
+public interface ShoppingCartDAO {
+    
+}
---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ShoppingCartFileDAO.java
@@ -0,0 +1,10 @@
+package com.estore.api.estoreapi.persistence;
+
+/**
+ * Implements the functionality for JSON file-based peristance for {@linkplain ShoppingCart ShoppingCarts}
+ * 
+ * @author Stevie Alvarez
+ */
+public class ShoppingCartFileDAO implements ShoppingCartDAO {
+    
+}
