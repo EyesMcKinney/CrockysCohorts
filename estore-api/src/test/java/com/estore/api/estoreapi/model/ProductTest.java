a613ad969f2e0e6d17fcb2ef068dd796e57647fc
hll7371 <hll7371@rit.edu>
2022-03-13 23:30:54 -0400

    branch made and file started

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ProductTest.java
@@ -0,0 +1,22 @@
+package com.estore.api.estoreapi.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Controller-tier")
+public class ProductTest {
+    
+    // CuT
+    private Product product;
+
+    /**
+     * Make the product before each test
+     */
+    @BeforeEach
+    void setup(){
+
+    }
+
+
+}
