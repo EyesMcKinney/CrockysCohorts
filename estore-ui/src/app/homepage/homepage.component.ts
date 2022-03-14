612a8442e10e8f5f0b3865732006a1a0dcc423d3
Tylin Hartman <78936590+tbh2677@users.noreply.github.com>
2022-03-14 18:09:23 -0400

    try again

--- estore-ui/src/app/homepage/homepage.component.ts
+++ estore-ui/src/app/homepage/homepage.component.ts
@@ -1,14 +1,27 @@
+/**
+ * Manages the website's homepage
+ * 
+ * @author Tylin Hartman
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { InventoryService } from '../inventory.service';
 
+
+/**
+ * Provides methods to handle inventory
+ * 
+ * @author Tylin Hartman
+ */
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: [ './homepage.component.css' ]
 })
 export class HomepageComponent implements OnInit {
-  products: Product[] = [];
+
+  products: Product[] = [];  // An array of products
 
   constructor(private inventoryService: InventoryService) { }
 
@@ -16,6 +29,10 @@ export class HomepageComponent implements OnInit {
     this.getProducts();
   }
 
+  /**
+   * Get an array of products from the server using 
+   * inventory Service
+   */
   getProducts(): void {
     this.inventoryService.getInventory()
       .subscribe(products => this.products = products);
