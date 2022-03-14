a9321bad9b11ec8fec342e7b282fda5829c8d549
hll7371 <hll7371@rit.edu>
2022-03-13 23:45:54 -0400

    all tests completed and pass

--- estore-api/src/test/java/com/estore/api/estoreapi/model/ProductTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ProductTest.java
@@ -1,5 +1,7 @@
 package com.estore.api.estoreapi.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -15,8 +17,45 @@ public class ProductTest {
      */
     @BeforeEach
     void setup(){
-
+        product = new Product(5, "shoe", 6.77, 10, "This product is a shoe");
     }
 
+    /**
+     * Test if set name changes the name correctly
+     */
+    @Test
+    void testSetName(){
+        // Invoke
+        String newName = "croc";
+        product.setName(newName);
 
+        // Check
+        assertEquals(newName, product.getName());
+    }
+
+    /**
+     * Test if set price changes the price correctly
+     */
+    @Test
+    void testSetPrice(){
+        // Invoke
+        double newPrice = 9.99;
+        product.setPrice(newPrice);
+
+        // Check
+        assertEquals(newPrice, product.getPrice());
+    }
+
+    /**
+     * Test if set description changes the description correctly
+     */
+    @Test
+    void testSetDescription(){
+        // Invoke
+        String newDescription = "This product is made of leather";
+        product.setDescription(newDescription);
+
+        // Check
+        assertEquals(newDescription, product.getDescription());
+    }
 }
